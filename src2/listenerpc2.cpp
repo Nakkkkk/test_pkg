398570760b800c3100910fa325dd516a83c1ae97
Nakkkkk <p238046k@mail.kyutech.jp>
2018-07-07 21:32:59 +0900

    2018/07/07true

--- src2/listenerpc2.cpp
+++ src2/listenerpc2.cpp
@@ -1,22 +1,33 @@
 #include "listenerpc2.h"
+#include <vector>
 
-void listenerpc2Callback(const sensor_msgs::PointCloud2& msg)
+//typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+
+void listenerpc2Callback(const sensor_msgs::PointCloud2ConstPtr& msg)
 {
+  pcl::PointCloud<pcl::PointXYZ> cloud;
+  pcl::fromROSMsg (*msg, cloud);
+
+  printf ("Cloud: width = %d, height = %d\n", cloud.width, cloud.height);
+  BOOST_FOREACH (const pcl::PointXYZ& pt, cloud.points)
+  printf ("\t(%f, %f, %f)\n", pt.x, pt.y, pt.z);
+
+/*
   // get width and height of 2D point cloud data
   int width = msg.width;
   int height = msg.height;
 
-  // Convert from u (column / width), v (row/height) to position in array
+  // Convert from u (column / width), v (row / height) to position in array
   // where X,Y,Z data starts
 
 //  int arrayPosition = v*msg.row_step + u*msg.point_step;
-  int arrayPosition = msg.row_step + msg.point_step;
+  int arrayPosition = (height-240)*msg.row_step + (width-320)*msg.point_step;
 
   // compute position in array where x,y,z data start
   int arrayPosX = arrayPosition + msg.fields[0].offset; // X has an offset of 0
   int arrayPosY = arrayPosition + msg.fields[1].offset; // Y has an offset of 4
   int arrayPosZ = arrayPosition + msg.fields[2].offset; // Z has an offset of 8
-/*
+
   float X = 0.0;
   float Y = 0.0;
   float Z = 0.0;
@@ -28,15 +39,27 @@ void listenerpc2Callback(const sensor_msgs::PointCloud2& msg)
   p.x = X;
   p.y = Y;
   p.z = Z;
-*/
+
   ROS_INFO("============================================");
-  ROS_INFO("X: [%u]", msg.data[arrayPosX]);
+  ROS_INFO("height: %d", msg.height);
+  ROS_INFO("width: %d", msg.width);
+  ROS_INFO("row_step: [%d]", msg.row_step);
+  ROS_INFO("point_step: [%d]", msg.point_step);
+  ROS_INFO("X: [%d]", msg.data[arrayPosX]);
   ROS_INFO("Y: [%u]", msg.data[arrayPosY]);
   ROS_INFO("Z: [%u]", msg.data[arrayPosZ]);
-
+  ROS_INFO("x:OFFSET = %d", msg.fields[0].offset);
+  ROS_INFO("y:OFFSET = %d", msg.fields[1].offset);
+  ROS_INFO("z:OFFSET = %d", msg.fields[2].offset);
+*/
+/*
+  std::vector<float> cloud_points_z;
+  float p[3] = read_points(msg, field_names = ("x", "y", "z"), skip_nans=false);
+  cloud_point_z.push_back()
+*/
 }
 
 void Listenerpc2::startListenerpc2()
 {
-  sub = n.subscribe("/camera/depth/points", 1000, listenerpc2Callback);
+  sub = n.subscribe<sensor_msgs::PointCloud2>("/camera/depth/points", 1000, listenerpc2Callback);
 }
--- src2/listenerpc2.h
+++ src2/listenerpc2.h
@@ -1,9 +1,16 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "sensor_msgs/PointCloud2.h"
-#include "sensor_msgs/PointField.h"
 
 #include <string.h>
+
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl_ros/point_cloud.h>
 
 class Listenerpc2
 {

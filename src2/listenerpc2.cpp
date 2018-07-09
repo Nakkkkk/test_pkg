2283c27c18ebbb28f1e15d24acd193f8c7e634e7
Nakkkkk <p238046k@mail.kyutech.jp>
2018-07-10 03:08:56 +0900

    2018/07/09

--- src2/listenerpc2.cpp
+++ src2/listenerpc2.cpp
@@ -6,8 +6,9 @@
 //void listenerpc2Callback(const sensor_msgs::PointCloud2& msg)
 void Listenerpc2::listenerpc2Callback(const sensor_msgs::PointCloud2ConstPtr& msg)
 {
+  sensor_msgs::PointCloud2 pc2;
   pcl::PointCloud<pcl::PointXYZ> cloud;
-//  pcl::PointCloud<pcl::uint8_t> cloud;
+  pc2 = *msg;
   pcl::fromROSMsg (*msg, cloud);
 
   printf ("Cloud: width = %d, height = %d\n", cloud.width, cloud.height);
@@ -18,50 +19,80 @@ void Listenerpc2::listenerpc2Callback(const sensor_msgs::PointCloud2ConstPtr& ms
   cloud.points.resize (cloud.width * cloud.height);
 
 //  pcl::io::savePCDFileASCII ("test_pcd.pcd", cloud);
-//  std::cerr << "Saved " << cloud.points.size () << " data points to test_pcd.pcd." << std::endl;
+  std::cerr << " size: " << cloud.points.size() << " cloud points" << std::endl;
+  std::cerr << " size: " << pc2.data.size() << " data points " << std::endl;
 
-  printf ("size is %d\n",cloud.points.size ());
+//  printf ("size is %d\n",cloud.points.size());
 //  for (size_t i = 0; i < cloud.points.size (); ++i)
 //    std::cerr << "    " << cloud.points[i].x << " " << cloud.points[i].y << " " << cloud.points[i].z << std::endl;
-//  std::cerr << "    " << cloud.points[(cloud.height/2)*cloud.width + cloud.width/2].x << " " << cloud.points[(cloud.height/2)*cloud.width + cloud.width/2].y << " " << cloud.points[(cloud.height/2)*cloud.width + cloud.width/2].z << std::endl;
+  std::cerr << "    " << cloud.points[(cloud.height/2)*cloud.width + cloud.width/2].x << " " << cloud.points[(cloud.height/2)*cloud.width + cloud.width/2].y << " " << cloud.points[(cloud.height/2)*cloud.width + cloud.width/2].z << std::endl;
 
 //std::cout << typeid(cloud.points[(cloud.height/2)*cloud.width + cloud.width/2].z).name() << std::endl;
 
-  sensor_msgs::PointCloud2 pc2;
-  printf("yea1\n");
-  pc2.height = 480;
-  pc2.width = 640;
+//  printf("yea1\n");
+//  pc2.height = 480;
+//  pc2.width = 640;
 //  pc2.fields[0].offset = 0;
 //  pc2.fields[1].offset = 4;
 //  pc2.fields[2].offset = 8;
 //  pc2.is_bigendian = false;
-  pc2.point_step = 16;
-  pc2.row_step = 10240;
+//  pc2.point_step = 16;
+//  pc2.row_step = 10240;
 //  pc2.is_dense = false;
-  printf("yea2\n");
+//  printf("yea2\n");
 //  std::vector<pcl::uint8_t> data;
-
-  for (size_t i = 0; i < cloud.points.size()*16; ++i){
 /*
-    if((cloud.height/2)*cloud.width <= i && i <= (cloud.height/2)*cloud.width + cloud.width - 1){
-      pc2.data.push_back(cloud.points[i].x);
-      pc2.data.push_back(cloud.points[i].y);
-      pc2.data.push_back(cloud.points[i].z);
+  for (size_t i = 0; i < cloud.points.size()*16; i=i+16){
+
+    if((cloud.height/2)*16*cloud.width <= i && i <= (cloud.height/2)*16*cloud.width + 16*cloud.width - 1){
+//      pc2.data.push_back(cloud.points[i].x);
+//      pc2.data.push_back(cloud.points[i].y);
+//      pc2.data.push_back(cloud.points[i].z);
+        pc2.data[i] =  cloud.points[i].x;
+        pc2.data[i + 4] =  cloud.points[i].y;
+        pc2.data[i + 8] =  cloud.points[i].z;
     }
-*/
+
 //    else{
-      pc2.data.push_back(0);
+//      pc2.data.push_back(0);
 //    }
   }
-
-  for (size_t i = 0; i < cloud.width*16; i=i+16){
-    pc2.data[(cloud.height/2)*16*cloud.width + i] =  cloud.points[i].x;
-    pc2.data[(cloud.height/2)*16*cloud.width + i + 4] =  cloud.points[i].y;
-    pc2.data[(cloud.height/2)*16*cloud.width + i + 8] =  cloud.points[i].z;
+*/
+/*
+  for (size_t i = 0 ; i < cloud.width*cloud.height; i=i+1){
+    if((cloud.height/2)*cloud.width > i && i >= (cloud.height/2)*cloud.width + cloud.width){
+      pc2.data[i*16] =  0;
+      pc2.data[i*16 + 4] =  0;
+      pc2.data[i*16 + 8] =  0;
+    }
   }
-
+*/
+/*
+  for (size_t i = 0 ; i < cloud.width; i=i+1){
+    pc2.data[(cloud.height/2)*16*cloud.width + i*16] =  cloud.points[i].x;
+    pc2.data[(cloud.height/2)*16*cloud.width + i*16 + 4] =  cloud.points[i].y;
+    pc2.data[(cloud.height/2)*16*cloud.width + i*16 + 8] =  cloud.points[i].z;
+  }
+*/
+/*
+  for (size_t i = (cloud.height/2)*16*cloud.width; i < (cloud.height/2)*16*cloud.width + 16*cloud.width ; i=i+16){
+    pc2.data[i] =  cloud.points[i].x;
+    pc2.data[i + 4] =  cloud.points[i].y;
+    pc2.data[i + 8] =  cloud.points[i].z;
+  }
+*/
 //  pub = n.advertise<sensor_msgs::PointCloud2>("laser_pc_cloud", 1);
-  pub.publish(pc2);
+
+  ROS_INFO("============================================");
+  ROS_INFO("height: %d", pc2.height);
+  ROS_INFO("width: %d", pc2.width);
+  ROS_INFO("row_step: [%d]", pc2.row_step);
+  ROS_INFO("point_step: [%d]", pc2.point_step);
+  ROS_INFO("x:OFFSET = %d", pc2.fields[0].offset);
+  ROS_INFO("y:OFFSET = %d", pc2.fields[1].offset);
+  ROS_INFO("z:OFFSET = %d", pc2.fields[2].offset);
+
+//  pub.publish(pc2);
 
 /*
   // get width and height of 2D point cloud data
